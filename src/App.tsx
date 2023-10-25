e89bb4c178f7a059831044c6779f4cbd64dc7d52
xokacar <okaykacar31@gmail.com>
2023-10-25 16:22:32 +0300

    chore: basic vite-react project files

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1>Hands on Docker ❤️‍🔥 </h1>
+        <iframe
+          src="https://giphy.com/embed/jTnGaiuxvvDNK"
+          width="480"
+          height="457"
+          frameBorder={0}
+          allowFullScreen
+          title="Funny Cat GIF"
+        ></iframe>
+      </div>
+    </>
+  );
+}
+
+export default App;
